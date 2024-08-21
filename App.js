2d00053aedca3aeea01e2f5a4e96816bc5f297d0
WilliamCallao <96638909+WilliamCallao@users.noreply.github.com>
2024-08-20 23:58:21 -0400

    Slide Transition

--- App.js
+++ App.js
@@ -1,18 +1,24 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createStackNavigator, TransitionPresets } from '@react-navigation/stack'; // Cambiado a createStackNavigator
 import { View, StyleSheet } from 'react-native';
 import MainScreen from './screens/MainScreen';
 import OtherScreen from './screens/OtherScreen';
 import FloatingNavbar from './FloatingNavbar';
 
-const Stack = createNativeStackNavigator();
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
       <View style={{ flex: 1 }}>
-        <Stack.Navigator initialRouteName="MainScreen" screenOptions={{ headerShown: false }}>
+        <Stack.Navigator
+          initialRouteName="MainScreen"
+          screenOptions={{
+            headerShown: false,
+            ...TransitionPresets.SlideFromRightIOS, // Ahora esto debería funcionar correctamente
+          }}
+        >
           <Stack.Screen name="MainScreen" component={MainScreen} />
           <Stack.Screen name="OtherScreen" component={OtherScreen} />
         </Stack.Navigator>
