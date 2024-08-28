ef863669870c2062a68ac70d5a930e06456baf02
WilliamCallao <96638909+WilliamCallao@users.noreply.github.com>
2024-08-28 18:26:18 -0400

    Firebase

--- screens/MainScreen.js
+++ screens/MainScreen.js
@@ -1,11 +1,34 @@
 // MainScreen.js
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
+import { db } from '../firebase';
+import { collection, getDocs } from 'firebase/firestore';
 
-export default function OtherScreen() {
+export default function MainScreen() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const querySnapshot = await getDocs(collection(db, 'usuarios'));
+      const dataList = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+      setData(dataList);
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Pantalla 1</Text>
+      <Text style={styles.title}>Lista de Usuarios</Text>
+      <FlatList
+        data={data}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <View style={styles.item}>
+            <Text>{item.nombre}</Text>
+          </View>
+        )}
+      />
     </View>
   );
 }
@@ -13,12 +36,19 @@ export default function OtherScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    paddingTop: 30,
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#D8E2FD',
   },
-  text: {
-    fontSize: 16,
-    color: '#000'
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginVertical: 10,
+  },
+  item: {
+    padding: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
   },
 });
