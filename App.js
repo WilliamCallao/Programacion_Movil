4045c630e8f7659f7a9993bbc8715559013f546b
WilliamCallao <96638909+WilliamCallao@users.noreply.github.com>
2024-10-11 15:52:45 -0400

    Update Package.json

--- App.js
+++ App.js
@@ -1,32 +1,16 @@
-import * as React from 'react';
+import React from 'react';
+import { View } from 'react-native';
+import AppNavigator from './src/navigation/AppNavigator';
+import FloatingNavbar from './src/components/BottomNavbar';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator, TransitionPresets } from '@react-navigation/stack';
-import { View, StyleSheet } from 'react-native';
-import MainScreen from './screens/MainScreen';
-import OtherScreen from './screens/OtherScreen';
-import OtherOtherScreen from './screens/OtherOtherScreen';
-import FloatingNavbar from './FloatingNavbar';
-
-const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
       <View style={{ flex: 1 }}>
-        <Stack.Navigator
-          initialRouteName="MainScreen"
-          screenOptions={{
-            headerShown: false,
-            ...TransitionPresets.SlideFromRightIOS,
-          }}
-        >
-          <Stack.Screen name="MainScreen" component={MainScreen} />
-          <Stack.Screen name="OtherScreen" component={OtherScreen} />
-          <Stack.Screen name="OtherOtherScreen" component={OtherOtherScreen} />
-        </Stack.Navigator>
+        <AppNavigator />
         <FloatingNavbar />
       </View>
     </NavigationContainer>
   );
 }
-
