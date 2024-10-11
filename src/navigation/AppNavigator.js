d4e43ad0bc2f9add1cc5ee143bc554d5bc31c4a0
WilliamCallao <96638909+WilliamCallao@users.noreply.github.com>
2024-10-11 15:48:43 -0400

    Create AppNavigator.js

---
+++ src/navigation/AppNavigator.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { createStackNavigator, TransitionPresets } from '@react-navigation/stack';
+import PlanScreen from '../screens/PlanScreen';
+import ProfileScreen from '../screens/ProfileScreen';
+import ProgressScreen from '../screens/ProgressScreen';
+import RecipesScreen from '../screens/RecipesScreen';
+
+const Stack = createStackNavigator();
+
+const AppNavigator = () => {
+  return (
+      <Stack.Navigator
+        initialRouteName="PlanScreen"
+        screenOptions={{
+          headerShown: false,
+          ...TransitionPresets.SlideFromRightIOS,
+        }}
+      >
+        <Stack.Screen name="PlanScreen" component={PlanScreen} />
+        <Stack.Screen name="ProfileScreen" component={ProfileScreen} />
+        <Stack.Screen name="ProgressScreen" component={ProgressScreen} />
+        <Stack.Screen name="RecipesScreen" component={RecipesScreen} />
+
+      </Stack.Navigator>
+  );
+};
+
+export default AppNavigator;
