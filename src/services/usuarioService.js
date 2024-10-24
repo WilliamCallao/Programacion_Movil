bf2e894f05ef3c626178d1d930c6dfe63c7074eb
WilliamCallao <96638909+WilliamCallao@users.noreply.github.com>
2024-10-23 20:09:16 -0400

    Update usuarioService.js

--- src/services/usuarioService.js
+++ src/services/usuarioService.js
@@ -93,7 +93,7 @@ export const crearUsuario = async (datosUsuario) => {
 
     await guardarIdEnAsyncStorage(usuarioRef.id);
 
-    const planSemanal = await generarPlanSemanal(datosUsuario.objetivos.meta_calorias);
+    const planSemanal = await generarPlanSemanal(datosUsuario.objetivos.meta_calorias, datosUsuario.preferencias);
 
     const fechaCreacion = new Date();
     const planesAlimentacion = planSemanal.map((plan, index) => ({
