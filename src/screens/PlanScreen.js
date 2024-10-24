3c9109d0b9e36609c3dfbe06ca7e4b5b41f678d4
WilliamCallao <96638909+WilliamCallao@users.noreply.github.com>
2024-10-24 08:52:48 -0400

    Update PlanScreen.js

--- src/screens/PlanScreen.js
+++ src/screens/PlanScreen.js
@@ -7,7 +7,7 @@ import { obtenerRecetasPorIds } from '../services/recetaService';
 import RecipeCard from '../components/RecipeCard';
 import Animated, { useSharedValue, useAnimatedScrollHandler } from 'react-native-reanimated';
 
-const SECTION_HEIGHT_PERCENTAGES = [5, 7, 9, 12, 14];
+const SECTION_HEIGHT_PERCENTAGES = [5, 7, 9, 16, 10];
 const COLORS = ['#FFF', '#33FF57', '#3357FF', '#FF33A6', '#F3FF33', '#33FFF5'];
 const USER_ID = 'Ieq3dMwGsdDqInbvlUvy';
 
@@ -135,7 +135,7 @@ export default function MainScreen() {
                 showsHorizontalScrollIndicator={false}
                 onScroll={scrollHandler}
                 scrollEventThrottle={16}
-                snapToInterval={ITEM_WIDTH}
+                snapToInterval={ITEM_WIDTH+16}
                 decelerationRate="fast"
                 contentContainerStyle={{ alignItems: 'center' }}
               />
@@ -156,8 +156,11 @@ const styles = StyleSheet.create({
   section5: {
     justifyContent: 'center',
     alignItems: 'center',
+    backgroundColor: 'green'
   },
-  section6: {},
+  section6: {
+    backgroundColor: 'blue'
+  },
   section7: {
     justifyContent: 'center',
     alignItems: 'center',
