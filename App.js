50d06d18b732b0ee4d28940715800b2d9c9af892
WilliamCallao <96638909+WilliamCallao@users.noreply.github.com>
2024-10-24 14:09:59 -0400

    DM - Sans

--- App.js
+++ App.js
@@ -3,21 +3,15 @@ import { View, ActivityIndicator } from 'react-native';
 import AppNavigator from './src/navigation/AppNavigator';
 import FloatingNavbar from './src/components/BottomNavbar';
 import { NavigationContainer } from '@react-navigation/native';
-import {
-  useFonts,
-  Poppins_100Thin,
-  Poppins_200ExtraLight,
-  Poppins_300Light,
-  Poppins_400Regular,
-  Poppins_500Medium,
-  Poppins_600SemiBold,
-  Poppins_700Bold,
-  Poppins_800ExtraBold,
-  Poppins_900Black,
+import { useFonts as usePoppins, Poppins_100Thin, Poppins_200ExtraLight,
+  Poppins_300Light, Poppins_400Regular, Poppins_500Medium, Poppins_600SemiBold,
+  Poppins_700Bold, Poppins_800ExtraBold, Poppins_900Black,
 } from '@expo-google-fonts/poppins';
+import {useFonts as useDMSans, DMSans_400Regular, DMSans_500Medium, DMSans_700Bold,
+} from '@expo-google-fonts/dm-sans';
 
 export default function App() {
-  let [fontsLoaded] = useFonts({
+  let [poppinsLoaded] = usePoppins({
     Poppins_100Thin,
     Poppins_200ExtraLight,
     Poppins_300Light,
@@ -28,9 +22,14 @@ export default function App() {
     Poppins_800ExtraBold,
     Poppins_900Black,
   });
-  
 
-  if (!fontsLoaded) {
+  let [dmSansLoaded] = useDMSans({
+    DMSans_400Regular,
+    DMSans_500Medium,
+    DMSans_700Bold,
+  });
+
+  if (!poppinsLoaded || !dmSansLoaded) {
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
         <ActivityIndicator size="large" color="#333" />
