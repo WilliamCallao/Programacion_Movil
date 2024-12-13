e3a41310b0a4b855d1e7813332a0d39ae9ff33be
WilliamCallao <96638909+WilliamCallao@users.noreply.github.com>
2024-12-13 08:52:35 -0400

    Update LoginScreen.js

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,19 +1,59 @@
-
-import React, { useState, useContext } from 'react';
-import { View, Text, TextInput, Button, StyleSheet, ScrollView, Alert, Animated, Easing } from 'react-native';
+import React, { useState, useContext, useRef, useEffect } from 'react';
+import { View, Text, TextInput, StyleSheet, ScrollView, Animated, Easing, TouchableOpacity, ActivityIndicator, Alert } from 'react-native';
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from '../services/firebase';
 import { AuthContext } from '../context/AuthContext';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { FontAwesome5 } from '@expo/vector-icons';
+import { FontAwesome5, MaterialIcons } from '@expo/vector-icons';
+
+const buttonDynamic = (isLoading) => ({
+  backgroundColor: isLoading ? '#555' : 'black',
+  borderColor: isLoading ? '#555' : 'black',
+  borderWidth: 1,
+  borderRadius: 8,
+  padding: 10,
+  alignItems: 'center',
+  justifyContent: 'center',
+});
+
+const textDynamic = (isLoading) => ({
+  color: '#fff',
+  fontSize: 14,
+  fontFamily: 'DMSans_500Medium',
+  textAlign: 'center',
+  opacity: isLoading ? 0 : 1,
+});
 
 const LoginScreen = ({ navigation }) => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
-  const { } = useContext(AuthContext);
+  const [showPassword, setShowPassword] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState(''); // Estado para manejar el mensaje de error
+  const spinValue = useRef(new Animated.Value(0)).current;
+
+  useEffect(() => {
+    Animated.loop(
+      Animated.timing(
+        spinValue,
+        {
+          toValue: 1,
+          duration: 4000,
+          easing: Easing.linear,
+          useNativeDriver: true
+        }
+      )
+    ).start();
+  }, [spinValue]);
+
+  const spin = spinValue.interpolate({
+    inputRange: [0, 1],
+    outputRange: ['0deg', '360deg']
+  });
 
   const handleLogin = async () => {
+    setIsLoading(true);
+    setErrorMessage(''); // Limpiar cualquier error previo
     try {
       const userCredential = await signInWithEmailAndPassword(auth, email, password);
       const user = userCredential.user;
@@ -25,33 +65,15 @@ const LoginScreen = ({ navigation }) => {
         console.log('Usuario ID almacenado en AsyncStorage:', user.uid);
       } catch (storageError) {
         console.error('Error al almacenar usuarioId en AsyncStorage:', storageError);
-        setError('No se pudo almacenar la información del usuario. Inténtalo nuevamente.');
+        setErrorMessage('No se pudo almacenar la información del usuario. Inténtalo nuevamente.');
       }
-
     } catch (error) {
       console.error('Authentication error:', error.message);
-      setError('Error de Autenticación: ' + error.message);
+      setErrorMessage('Credenciales incorrectas. Por favor, inténtalo de nuevo.'); // Actualiza el mensaje de error
+    } finally {
+      setIsLoading(false);
     }
   };
-
-  // Animación para los iconos
-  const spinValue = new Animated.Value(0);
-  Animated.loop(
-    Animated.timing(
-      spinValue,
-      {
-        toValue: 1,
-        duration: 4000,
-        easing: Easing.linear,
-        useNativeDriver: true
-      }
-    )
-  ).start();
-
-  const spin = spinValue.interpolate({
-    inputRange: [0, 1],
-    outputRange: ['0deg', '360deg']
-  });
 
   return (
     <ScrollView contentContainerStyle={styles.container}>
@@ -60,7 +82,6 @@ const LoginScreen = ({ navigation }) => {
       </Animated.View>
       <View style={styles.authContainer}>
         <Text style={styles.title}>Iniciar Sesión</Text>
-        {error ? <Text style={styles.errorText}>{error}</Text> : null}
         <TextInput
           style={styles.input}
           value={email}
@@ -69,16 +90,37 @@ const LoginScreen = ({ navigation }) => {
           autoCapitalize="none"
           keyboardType="email-address"
         />
-        <TextInput
-          style={styles.input}
-          value={password}
-          onChangeText={setPassword}
-          placeholder="Contraseña"
-          secureTextEntry
-        />
-        <View style={styles.buttonContainer}>
-          <Button title="Iniciar Sesión" onPress={handleLogin} color="#3498db" />
+        <View style={styles.passwordContainer}>
+          <TextInput
+            style={[styles.inputPass, { flex: 1 }]}
+            value={password}
+            onChangeText={setPassword}
+            placeholder="Contraseña"
+            secureTextEntry={!showPassword}
+          />
+          <TouchableOpacity onPress={() => setShowPassword(!showPassword)}>
+            <MaterialIcons
+              name={showPassword ? "visibility" : "visibility-off"}
+              size={24}
+              color="#929292"
+            />
+          </TouchableOpacity>
         </View>
+
+        {/* Mostrar mensaje de error */}
+        {errorMessage ? <Text style={styles.errorText}>{errorMessage}</Text> : null}
+
+        <TouchableOpacity
+          style={[styles.dynamicButton, buttonDynamic(isLoading)]}
+          onPress={handleLogin}
+          disabled={isLoading}
+        >
+          {isLoading ? (
+            <ActivityIndicator size="small" color="#fff" />
+          ) : (
+            <Text style={textDynamic(isLoading)}>Iniciar Sesión</Text>
+          )}
+        </TouchableOpacity>
         <View style={styles.bottomContainer}>
           <Text style={styles.toggleText} onPress={() => navigation.navigate('RegisterScreen')}>
             ¿Necesitas una cuenta? Regístrate
@@ -121,8 +163,31 @@ const styles = StyleSheet.create({
     borderRadius: 8,
     fontSize: 16,
   },
-  buttonContainer: {
+  inputPass: {
+    height: 50,
+    padding: 12,
+    borderRadius: 8,
+    fontSize: 16,
+  },
+  passwordContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    borderColor: '#ddd',
+    borderWidth: 1,
+    borderRadius: 8,
+    paddingRight: 12,
     marginBottom: 16,
+  },
+  dynamicButton: {
+    marginBottom: 16,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  errorText: {
+    color: 'red',
+    fontSize: 14,
+    marginBottom: 16,
+    textAlign: 'center',
   },
   toggleText: {
     color: '#3498db',
@@ -131,11 +196,6 @@ const styles = StyleSheet.create({
   },
   bottomContainer: {
     marginTop: 20,
-  },
-  errorText: {
-    color: 'red',
-    marginBottom: 16,
-    textAlign: 'center',
   },
 });
 
